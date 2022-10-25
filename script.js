8d0cd4f3541e3121b8f5f6afb5e30fb9a3d9717f
“Rebecca <rebeccaschiffman@gmail.com>
2022-10-24 17:43:34 -0700

    Work on insertNode method more

--- script.js
+++ script.js
@@ -8,38 +8,230 @@ const nodeFactory = (data) => {
 
 // TREE CLASS
 const Tree = class {
+
   constructor(array) {
     this.array = array;
-    this.root = buildTree(array);
+    this.root = this.buildTree(array);
   }
 
-  //methods
-  insertNode(data) {
-    console.log(`data is ${data} and root is ${this.root.data}`);
-    if (data < this.root.data) {
-      console.log(`Data ${data} is smaller than this.root ${this.root}`);
-      if (this.root.left===null) {
-        this.root.left.data = data;
-        return this.root;
-      } else {
-      this.root.left(insertNode(data)) 
-      }
-    } else if (data > this.root.data) {
-      console.log(`Data ${data} is greater than this.root ${this.root.data}`);
-      if (this.root.right===null) {
-        this.root.right.data = data;
-        return this.root;
-      } else {
-        // something not right here
-      this.root.right(insertNode(data)) 
-      }
+// Methods
+  buildTree(array) {
+    let start = 0;
+    let end = array.length-1;
+    if (start > end) {
+      return null;
     } else {
-      console.log('no change made');
-      return this.root;
+      let mid = Math.floor(((start + end)/2));
+      const root = nodeFactory(array[mid]);
+      let leftArray = array.slice(start, mid);
+      let rightArray = array.slice(mid+1);
+      root.left = this.buildTree(leftArray);
+      root.right = this.buildTree(rightArray); 
+      return root;
     }
   }
 
-  // deleteNode() {
+  insertNode(data, currentNode = this.root) {
+    // base case
+    if (currentNode === null) {
+      currentNode = nodeFactory(data);
+    }
+    if (data === currentNode.data) {
+      return;
+    }
+    if (data < currentNode.data) {
+      currentNode.left = this.insertNode(data, currentNode.left);
+    } else if (data > currentNode.data) {
+      currentNode.right = this.insertNode(data, currentNode.right);
+    }
+
+    console.log(currentNode);
+    return currentNode;
+  }
+
+
+  // insertNode(data, current = this.root) {
+  //   console.log(`data is ${data} and current is ${current} and current.data is ${current.data}`)
+  //   if (data < current.data) {
+  //     console.log(`data ${data} is smaller than current.data ${current.data}`);
+  //     if (current.left===null) {
+  //       console.log(`current.leff is null so we will create a new node`)
+  //       // create node
+  //       const newNode = nodeFactory(data);
+  //       newNode.left = null;
+  //       newNode.right = null;
+  //       // point current leaf node to it
+  //       current.left = newNode;
+  //       return;
+  //     } else {
+  //     console.log(`current.left.data is ${current.left.data}`);
+  //     current = current.left;
+  //     // this does not work:
+  //     current.insertNode(data, current);
+  //     }
+  //   } else if (data > this.root.data) {
+  //     // if this is a leaf node
+  //     if (current.right===null) {
+  //       // create node
+  //       const newNode = nodeFactory(data);
+  //       newNode.left = null;
+  //       newNode.right = null;
+  //       // point current leaf node to it
+  //       current.right = newNode;
+  //       return;
+  //     } else {
+  //       current = current.right;
+  //       current.insertNode(data, current);
+  //     }
+  //   } else {
+  //     console.log('no change made');
+  //     return current;
+  //   }
+  // }
+
+
+  // alternate version after watching video
+  // addData(data) {
+  //   if (this.root === null) {
+  //     this.root = nodeFactory(data);
+
+  //   } else {
+  //     const insertNode = function(data, current = this.root){
+
+  //       if (data < current.data) {
+
+  //         if (current.left === null) {
+  //           return current.left = nodeFactory(data);
+  //         } else {
+  //           current = current.left;
+  //           return current.insertNode(data, current);
+  //         }
+
+  //       } else if (data > current.data) {
+  //         if (current.right === null) {
+  //           return current.right = nodeFactory(data);
+  //         } else {
+  //           current = current.right;
+  //           return current.insertNode(data, current);
+  //         }
+  //       }
+
+  //     }
+  //     insertNode(data);
+  //   }
+  // }
+
+
+
+
+}
+
+
+
+
+const TheTree = new Tree([1,3,5,7,9]);
+
+
+
+
+
+
+
+//   insertNode(data, current = this.root) {
+//     console.log(`data is ${data} and current is ${current} and current.data is ${current.data}`)
+//     if (data < current.data) {
+//       console.log(`data ${data} is smaller than current.data ${current.data}`);
+//       if (current.left===null) {
+//         console.log(`current.leff is null so we will create a new node`)
+//         // create node
+//         const newNode = nodeFactory(data);
+//         newNode.left = null;
+//         newNode.right = null;
+//         // point current leaf node to it
+//         current.left = newNode;
+//         return;
+//       } else {
+//       console.log(`current.left.data is ${current.left.data}`);
+//       current = current.left;
+//       // this does not work:
+//       current.insertNode(data, current);
+//       }
+//     } else if (data > this.root.data) {
+//       // if this is a leaf node
+//       if (current.right===null) {
+//         // create node
+//         const newNode = nodeFactory(data);
+//         newNode.left = null;
+//         newNode.right = null;
+//         // point current leaf node to it
+//         current.right = newNode;
+//         return;
+//       } else {
+//         current = current.right;
+//         current.insertNode(data, current);
+//       }
+//     } else {
+//       console.log('no change made');
+//       return current;
+//     }
+//   }
+// }
+
+
+
+// // BUILD TREE FUNCTION
+// const buildTree = function(array) {
+//   let start = 0;
+//   let end = array.length-1;
+//   if (start > end) {
+//     return null;
+//   } else {
+//     let mid = Math.floor(((start + end)/2));
+  
+//     const root = nodeFactory(array[mid]);
+//     let leftArray = array.slice(start, mid);
+//     let rightArray = array.slice(mid+1);
+
+//     // prettyPrint(root);
+
+//     // root.left = new Tree(leftArray);
+//     // root.right = new Tree(rightArray);
+
+//     root.left = buildTree(leftArray);
+//     root.right = buildTree(rightArray); 
+    
+//     return root;
+//   }
+// }
+
+// const prettyPrint = (node, prefix = '', isLeft = true) => {
+//   // if the node has a right child
+//   if (node.right !== null) {
+//     prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
+//   }
+//   console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
+//   // if the node has a left child
+//   if (node.left !== null) {
+//     prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
+//   }
+// }
+
+
+
+
+
+
+
+
+// const TheTree = new Tree([1,3,5]);
+
+
+
+// root.left = new Tree(leftArray);
+    // root.right = new Tree(rightArray);
+
+
+// deleteNode() {
     // if node is a leaf
       // the node that previously pointed to that node will no longer do that
     // else if node has one child, replace it with its child so node that previously pointed to it will point to its child
@@ -49,42 +241,3 @@ const Tree = class {
       // replace node with that node - recursively if something also had children??
 
   // }
-}
-
-  
-
-// BUILD TREE FUNCTION
-const buildTree = function(array) {
-  let start = 0;
-  let end = array.length-1;
-  if (start > end) {
-    return null;
-  } else {
-    let mid = Math.floor(((start + end)/2));
-  
-    const root = nodeFactory(array[mid]);
-    let leftArray = array.slice(start, mid);
-    let rightArray = array.slice(mid+1);
-
-    root.left = buildTree(leftArray);
-    root.right = buildTree(rightArray); 
-
-    prettyPrint(root);
-
-    return root;
-  }
-}
-
-const prettyPrint = (node, prefix = '', isLeft = true) => {
-  // if the node has a right child
-  if (node.right !== null) {
-    prettyPrint(node.right, `${prefix}${isLeft ? '│   ' : '    '}`, false);
-  }
-  console.log(`${prefix}${isLeft ? '└── ' : '┌── '}${node.data}`);
-  // if the node has a left child
-  if (node.left !== null) {
-    prettyPrint(node.left, `${prefix}${isLeft ? '    ' : '│   '}`, true);
-  }
-}
-
-const TheTree = new Tree([1,2,3,4,5,7,8]);
